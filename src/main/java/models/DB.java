59ed0b9b9ddc88881f146f7404a78de2a65af30d
ewanyama2019 <wafulawanyama@gmail.com>
2019-06-07 13:11:12 +0300

    Added in [java/main] a dao package then created dapartmentDao Sql2oDepartmentDao, then Created DB inside models package

---
+++ src/main/java/models/DB.java
@@ -0,0 +1,37 @@
+package models;
+
+import org.sql2o.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static spark.route.HttpMethod.connect;
+
+public class DB {
+
+    private static URI dbUri;
+
+    public static Sql2o sql2o; // = new Sql2o("jdbc:postgresql://localhost:5432/site_maintenance", "postgres", "wanyama123");
+
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+
+    static {
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/site_maintenance");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "postgres" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "wanyama123" : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e) {
+            logger.error("Unable to connect to database.");
+        }
+    }
+}
