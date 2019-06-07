f4a16f266ec32c39991f69f1534a622ecb7f9730
ewanyama2019 <wafulawanyama@gmail.com>
2019-06-07 18:30:00 +0300

    Added procfile

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,19 +17,19 @@ public class App {
 
 
 
-//        ProcessBuilder process = new ProcessBuilder();
-//        Integer port;
-//
-//        // This tells our app that if Heroku sets a port for us, we need to use that port.
-//        // Otherwise, if they do not, continue using port 4567.
-//
-//        if (process.environment().get("PORT") != null) {
-//            port = Integer.parseInt(process.environment().get("PORT"));
-//        } else {
-//            port = 4567;
-//        }
-//
-//        port(port);
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
 
 
         staticFileLocation("/public");
