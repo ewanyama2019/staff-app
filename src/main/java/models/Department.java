ca174d606870aca0c3bf1bbd4cbc6f39b16f72a7
ewanyama2019 <wafulawanyama@gmail.com>
2019-06-07 15:40:09 +0300

    added Constructor 'Department' and initialises private variables, Getters and setter. then added overide for checking new instance of Department

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,38 @@
 package models;
 
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Department {
+
+    private String name;
+    private int id;
+
+    //-----Department constructor
+    public Department(String name) {
+        this.name = name;
+    }
+
+    // -------------- GETTERS ------------------------
+    public String getName() {
+        return name;
+    }
+
+    //------------- SETTERS -------------------------
+    public void setName(){
+        this.name = name;
+    }
+
+    Override
+    public boolean equals(Object otherDepartment) {
+        if (!(otherDepartment instanceof Department)) {
+            return false;
+        } else {
+            Department newDepartment = (Department) otherDepartment;
+            return this.getName().equals(newDepartment.getName());
+        }
+    }
+
 }
