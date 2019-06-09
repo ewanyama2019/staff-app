55a95afa2a20d125d1d0d748f6aff1b0cd7dec94
ewanyama2019 <wafulawanyama@gmail.com>
2019-06-09 16:44:28 +0300

    Added tests for StaffMember getUser_name, getDepartment_ID and tested for creation of duplicate instances of StaffMember object. then Added basic StaffMemberDao Interface

--- src/main/java/models/StaffMember.java
+++ src/main/java/models/StaffMember.java
@@ -7,61 +7,106 @@ import java.util.Objects;
 
 public class StaffMember {
 
+    private String user_name;
     private String first_name;
     private String middle_name;
     private String sir_name;
     private int departmentID;
     private int id;
 
-    // -------------- GETTERS ------------------------
-    public StaffMember(String first_name) {
-        this.first_name = first_name;
+
+
+    // -----------constructor  StaffMember ----------------------
+    public StaffMember(String user_name, int departmentID) {
+        this.first_name = user_name;
+        this.departmentID = departmentID;
     }
 
+
+    // -------------- GETTERS ------------------------
+
+    public String getUser_name() {
+        return user_name;
+    }
 
     public String getFirst_name() {
         return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
     }
 
     public String getMiddle_name() {
         return middle_name;
     }
 
-    //------------- SETTERS -------------------------
-    public void setMiddle_name(String middle_name) {
-        this.middle_name = middle_name;
-    }
-
     public String getSir_name() {
         return sir_name;
-    }
-
-    public void setSir_name(String sir_name) {
-        this.sir_name = sir_name;
     }
 
     public int getDepartmentID() {
         return departmentID;
     }
 
+
+    //------------- SETTERS -------------------------
+
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setMiddle_name(String middle_name) {
+        this.middle_name = middle_name;
+    }
+
+    public void setSir_name(String sir_name) {
+        this.sir_name = sir_name;
+    }
+
     public void setDepartmentID(int departmentID) {
         this.departmentID = departmentID;
     }
 
+    // -----------Override Method --------------------------
+
     @Override
-    public boolean equals(Object otherStaffMember) {
-        if (this == otherStaffMember) return true;
-        if (!(otherStaffMember instanceof StaffMember)) return false;
-        StaffMember that = (StaffMember) otherStaffMember;
-        return getFirst_name().equals(that.getFirst_name());
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StaffMember)) return false;
+        StaffMember that = (StaffMember) o;
+        return getDepartmentID() == that.getDepartmentID() &&
+                Objects.equals(getUser_name(), that.getUser_name());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getFirst_name());
+        return Objects.hash(getUser_name(), getDepartmentID());
     }
+
+
+//    @Override
+//    public boolean equals(Object otherStaffMember) {
+//        if (this == otherStaffMember) return true;
+//        if (!(otherStaffMember instanceof StaffMember)) return false;
+//        StaffMember that = (StaffMember) otherStaffMember;
+//        return getDepartmentID() == that.getDepartmentID() &&
+//                getUser_name().equals(that.getUser_name());
+//    }
+
+//    @Override
+//    public boolean equals(Object otherStaffMember) {
+//        if (!(otherStaffMember instanceof StaffMember)) {
+//            return false;
+//        } else {
+//            StaffMember newStaffMember = (StaffMember) otherStaffMember;
+//            return this.getUser_name().equals(newStaffMember.getUser_name())  && this.getDepartmentID() == newStaffMember.getDepartmentID();
+//        }
+//    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getUser_name(), getDepartmentID());
+//    }
 }
--- src/test/java/models/StaffMemberTest.java
+++ src/test/java/models/StaffMemberTest.java
@@ -1,4 +1,39 @@
 package models;
 
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
+
 public class StaffMemberTest {
+
+//    @Rule
+//    public DatabaseRule databaseRule = new DatabaseRule();
+
+    @Test
+    public void staffMember_instantiatesCorrectly_True() {
+         StaffMember testStaffMember = new StaffMember("ewanyama0001", 1);
+        assertEquals(true, testStaffMember instanceof StaffMember);
+    }
+
+    @Test
+    public void staffMember_InstantiatesWithFirstName_String() {
+        StaffMember testStaffMember = new StaffMember("ewanyama0001", 1);
+        assertEquals("ewanyama0001", testStaffMember.getFirst_name());
+    }
+
+    @Test
+    public void Site_InstantiatesWithDepartmentId_int() {
+        StaffMember testStaffMember = new StaffMember("ewanyama0001", 1);
+        assertEquals(1, testStaffMember.getDepartmentID());
+    }
+
+    @Test
+    public void equals_returnsTrueIfUserNameAndDepartmentIdAreSame_true() {
+        StaffMember testStaffMember = new StaffMember("ewanyama0001", 1);
+        StaffMember anotherStaffMember = new StaffMember("ewanyama0001", 1);
+        assertTrue(testStaffMember.equals(anotherStaffMember));
+    }
+
+
 }
