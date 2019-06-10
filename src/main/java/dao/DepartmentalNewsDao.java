0209b4d9921c734ab67ce64a1246fd6267cb7fac
ewanyama2019 <wafulawanyama@gmail.com>
2019-06-10 04:06:58 +0300

    Added CRUD methods in interface DepartmentalNewsDao

--- src/main/java/dao/DepartmentalNewsDao.java
+++ src/main/java/dao/DepartmentalNewsDao.java
@@ -1,4 +1,25 @@
 package dao;
 
+import models.DepartmentalNews;
+import java.util.List;
+
 public interface DepartmentalNewsDao {
+
+    ///   LIST ALL NEWS HEADLINES
+    List<DepartmentalNews> listAllDepartmentalNews();
+
+    // CREATE :SAVE NEWS HEADLINES TO DATABASE
+    void saveDepartmentalNews(DepartmentalNews departmentalNews);
+
+    // READ NEWS HEADLINES BY ID
+    DepartmentalNews findDepartmentalNewsById(int id);
+
+    // UPDATE NEWS HEADLINES
+    void updateDepartmentalNews(String news_headlines, String news_details, int department_id);
+
+    // DELETE NEWS HEADLINES BY ID
+    void deleteDepartmentalNewsById(int id);
+
+    // DELETE NEWS HEADLINES
+    void deleteAllDepartmentalNews();
 }
